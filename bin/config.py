6a97ec7ae93a213a8976cc76c268937aec77daab
Denny Reiter <denny@reiters.org>
2020-07-25 09:44:43 -0500

    add the config.py file since it has the MQTT connection details

---
+++ bin/config.py
@@ -0,0 +1,10 @@
+# Config section
+# Uncomment these lines if your MQTT server requires authentication
+MQTT_USER="rtl433"
+MQTT_PASS="rtl-sdr"
+MQTT_HOST="192.168.0.77"
+MQTT_PORT=1883
+MQTT_TOPIC="sensors/rtl_433"
+MQTT_QOS=0
+DEBUG=1
+# End config section
